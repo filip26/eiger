557851f7076afbf0643807fd0c75be9aa1e2bbee
Filip <filip26@gmail.com>
2020-08-04 00:07:37 +0200

    fix XPathUtil

--- alps-xml/src/main/java/com/apicatalog/alps/xml/XPathUtil.java
+++ alps-xml/src/main/java/com/apicatalog/alps/xml/XPathUtil.java
@@ -6,14 +6,22 @@ import java.util.stream.Collectors;
 class XPathUtil {
     
     public static final String getPath(Deque<XmlElement> stack) {
-        return "/" + stack.stream().map(XmlElement::getElementName).collect(Collectors.joining("/"));
+        return stack.stream().map(XPathUtil::getElementPath).collect(Collectors.joining());
     }
 
     public static final String getPath(Deque<XmlElement> stack, final String element, final int index) {
-        return "/" 
-                + stack.stream().map(XmlElement::getElementName).collect(Collectors.joining("/")) 
-                + "/" + element
-                + (index != -1 ? "[position()=" + (index + 1) + "]" : "");
+        return  stack.stream().map(XPathUtil::getElementPath).collect(Collectors.joining()) 
+                + getElementPath(element, index)
+                ;
+    }
+
+    private static final String getElementPath(XmlElement element) {
+        return getElementPath(element.getElementName(), element.getElementIndex());
+    }
+
+    private static final String getElementPath(final String elementName, final int elementIndex) {
+        return "/" + elementName 
+                + (elementIndex != -1 ? "[position()=" + (elementIndex + 1) + "]" : ""); 
     }
 
     private XPathUtil() {
