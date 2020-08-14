b4459f93e2ddc960680fd865589816e3e6e03e66
Filip <filip26@gmail.com>
2020-08-14 17:53:08 +0200

    cli: improve yaml output

--- alps-cli/src/main/java/com/apicatalog/alps/cli/PrintUtils.java
+++ alps-cli/src/main/java/com/apicatalog/alps/cli/PrintUtils.java
@@ -25,41 +25,45 @@ final class PrintUtils {
     }
     
     static final void printError(PrintStream output, String path, DocumentException e) {
-        
-        output.println("valid: false");
-        
+
+        output.println("# Invalid ALPS document");
+        output.println("- error:");
+        output.println("    message: " + e.getMessage());
+
         if (e instanceof MalformedDocumentException) {
             
             final MalformedDocumentException me = (MalformedDocumentException)e;
             
-            output.println("error:");
-            output.println("  message: " + me.getMessage());
-            output.println("  location:");
-            output.println("    line: " + me.getLineNumber());
-            output.println("    column: " + me.getColumnNumber());
+            output.println("    location:");
+            output.println("      line: " + me.getLineNumber());
+            output.println("      column: " + me.getColumnNumber());
 
         } else if (e instanceof InvalidDocumentException) {
             
             final InvalidDocumentException ie = (InvalidDocumentException)e;
-            output.println("error:");
-            output.println("  message: " + ie.getMessage());
             
             if (ie.getPath() != null) {
                 output.println("  path:" + ie.getPath());
-            }
-            
-        } else {
-            output.println("error: " + e.getMessage());
+            }            
         }
     }
     
-    static final void printDocInfo(PrintStream output, Document document) {
-        output.println("valid: true");
-        output.println("alps: ");
-        output.println("  version: " + versionToString(document.getVersion()));
-        output.println("  descriptors:");
-        output.println("    top_level: " + document.getDescriptors().size());
-        output.println("    total: " + document.getAllDescriptors().size());
+    static final void printDocInfo(final PrintStream output, final Document document, final String mediaType, final String filePath) {
+        output.println("# Valid ALPS document");
+        output.println("- document: ");
+
+        if (mediaType != null) {
+            output.println("    media_type: " + mediaType);
+        }
+
+        if (filePath != null) {
+            output.println("    file: " + filePath);
+        }
+        
+        output.println("    version: " + versionToString(document.getVersion()));
+        output.println("    descriptors:");
+        output.println("      top_level: " + document.getDescriptors().size());
+        output.println("      total: " + document.getAllDescriptors().size());
     }
     
     private static final String versionToString(DocumentVersion version) {
