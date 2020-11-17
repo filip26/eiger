1bba177fe2cc445d0b73c589824ee4d0d05aa527
Filip <filip26@gmail.com>
2020-11-17 15:22:18 +0100

    alps.sh checks if Java is installed and installed Java version

--- alps-cli/bin/alps.sh
+++ alps-cli/bin/alps.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+JAVA_VERSION=$(java -version 2>&1 | head -1 | cut -d'"' -f2 | sed '/^1\./s///' | cut -d'.' -f1)
+
+if [ -z "$JAVA_VERSION" ]; then
+  echo "Java is not installed or java commnand in not accessible. Please install Java 11 or greater."
+  exit 1
+fi
+
+if [ "$JAVA_VERSION" -lt 11 ]; then
+  echo "alps.sh requires Java 11 or greater. Installed Java version is $JAVA_VERSION"
+  exit 1
+fi
+
 ALPS_JAR_NAME="alps-cli-*.jar"
 
 if [ -z "$ALPS_HOME" ]; then
