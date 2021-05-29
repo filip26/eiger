7b8919386626dc587a67f0711a968b6f4d28cd60
Filip <filip26@gmail.com>
2021-05-29 18:50:52 +0200

    Fix issue #106

--- service/src/main/webapp/src/Viewer.js
+++ service/src/main/webapp/src/Viewer.js
@@ -1,5 +1,10 @@
 import React from 'react'
 
+import { IconButton, Snackbar } from '@material-ui/core';
+
+import MuiAlert from '@material-ui/lab/Alert';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
+
 import { makeStyles } from '@material-ui/core/styles';
 
 import {Controlled as CodeMirror} from 'react-codemirror2'
@@ -7,13 +12,27 @@ import {Controlled as CodeMirror} from 'react-codemirror2'
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    fontSize: '16px'
-  }
+    fontSize: '16px',
+    position: 'relative'
+  },
+  copy: {
+    position: 'absolute',
+    bottom: theme.spacing(2),
+    right: theme.spacing(4),
+    zIndex: 200,
+    color: theme.palette.text.secondary,
+  },
 }));
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 
 export default function Viewer(props) {
 
     const classes = useStyles();
+
+    const [open, setOpen] = React.useState(false);
 
     const { mediaType, value } = props;
 
@@ -36,17 +55,44 @@ export default function Viewer(props) {
         }
         return "text";
     }
+    
+    const handleCopy =  event => {
+        navigator.clipboard.writeText(value);
+        setOpen(true);
+    };
+    
+    const handleClose = (event, reason) => {
+      if (reason === 'clickaway') {
+        return;
+      }
+
+      setOpen(false);
+    };
 
     return (
-        <CodeMirror
-            className={classes.root}
-            value={value}
-            options={{
-                mode: typeToMode(mediaType),
-                theme: 'material-darker',
-                lineNumbers: false,
-                readOnly: 'nocursor',
-                }}
-            />
-            );
+      <div>
+        <div className={classes.root}>
+          <IconButton className={classes.copy} onClick={handleCopy}>
+            <FileCopyIcon/>
+          </IconButton>
+          <CodeMirror
+              value={value}
+              options={{
+                  mode: typeToMode(mediaType),
+                  theme: 'material-darker',
+                  lineNumbers: false,
+                  readOnly: 'nocursor',
+                  }}
+              />
+
+        </div>
+        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+          <div>
+             <Alert onClose={handleClose} severity="success">
+                Result has been copied to a clipboard.
+              </Alert>
+          </div>
+        </Snackbar>
+      </div>
+      );
 }
